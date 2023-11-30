127797a1ac5b79465113b9d3cc9508bcc5e91b8c
ChancellorO <chancellororichey@gmail.com>
2023-11-29 22:16:41 -0800

    Added Charts and static data

--- mobile/Features/Feed/Screens/Feed.jsx
+++ mobile/Features/Feed/Screens/Feed.jsx
@@ -4,11 +4,71 @@ import {
 } from 'react-native';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import { BarChart, LineChart } from 'react-native-gifted-charts';
 import Post from '../Components/Post';
 
 export default function Feed({ navigation }) {
   const [postDraftBody, setPostDraftBody] = useState('');
   const [postDraftUser, setPostDraftUser] = useState('');
+  const [newNum, setNewNum] = useState();
+  const [data, setData] = useState([
+    {
+      value: 10,
+      timestamp: '2023-10-24T00:41:59.057Z',
+    },
+    {
+      value: 11,
+      timestamp: '2023-10-23T00:41:59.057Z',
+    },
+    {
+      value: 12,
+      timestamp: '2023-10-23T00:41:59.057Z',
+    },
+    {
+      value: 13,
+      timestamp: '2023-10-23T00:41:59.057Z',
+    },
+    {
+      value: 14,
+      timestamp: '2023-10-23T00:41:59.057Z',
+    }]);
+
+  const barData = [
+    {
+      value: 230,
+      label: 'Jan',
+      frontColor: '#4ABFF4',
+      sideColor: '#23A7F3',
+      topColor: '#92e6f6',
+    },
+    {
+      value: 180,
+      label: 'Feb',
+      frontColor: '#79C3DB',
+      sideColor: '#68BCD7',
+      topColor: '#9FD4E5',
+    },
+    {
+      value: 195,
+      label: 'Mar',
+      frontColor: '#28B2B3',
+      sideColor: '#0FAAAB',
+      topColor: '#66C9C9',
+    },
+    {
+      value: 250,
+      label: 'Apr',
+      frontColor: '#4ADDBA',
+      sideColor: '#36D9B2',
+      topColor: '#7DE7CE',
+    },
+    {
+      value: 320,
+      label: 'May',
+      frontColor: '#91E3E3',
+      sideColor: '#85E0E0',
+      topColor: '#B0EAEB',
+    }];
 
   // const GIVEN_POSTS = [
   //   {
@@ -55,10 +115,10 @@ export default function Feed({ navigation }) {
     foo();
   }, []);
 
-  const pushPosts = async (data) => {
+  const pushPosts = async (rdata) => {
     const foo = async () => {
       try {
-        await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/posts/createPost`, data);
+        await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/posts/createPost`, rdata);
         // console.log(res.data);
       } catch (err) {
         console.log(err);
@@ -94,9 +154,24 @@ export default function Feed({ navigation }) {
     });
   };
 
+  const addNewNum = (e) => {
+    e.preventDefault();
+    const tempPostList = data;
+    tempPostList.push({
+      value: newNum,
+      timestamp: new Date().toDateString(),
+    });
+    setData(tempPostList);
+    setNewNum('');
+  };
+
   const onChangeDraftBody = (val) => {
     // console.log(val);
     setPostDraftBody(val);
+  };
+
+  const onChangeNewNum = (val) => {
+    setNewNum(val);
   };
 
   const onChangeDraftUser = (val) => {
@@ -118,6 +193,51 @@ export default function Feed({ navigation }) {
         ),
       )}
       <Text> Demo Form </Text>
+      <View>
+        <BarChart
+          showFractionalValue
+          showYAxisIndices
+          hideRules
+          noOfSections={4}
+          maxValue={400}
+          data={barData}
+          barWidth={40}
+          sideWidth={15}
+          isThreeD
+          side="right"
+        />
+      </View>
+      <View>
+        <LineChart
+          data={data}
+          width={300}
+          height={300}
+        />
+      </View>
+      <View>
+        <TextInput
+          name="value"
+          placeholder="value"
+          value={newNum}
+          onChangeText={onChangeNewNum}
+        />
+        <Button
+          title="AddNewNum"
+          onPress={addNewNum}
+        />
+      </View>
+      <View>
+        <TextInput
+          name="startdate"
+          placeholder=""
+          value={newNum}
+          onChangeText={onChangeNewNum}
+        />
+        <Button
+          title="AddNewNum"
+          onPress={addNewNum}
+        />
+      </View>
       <View>
         <TextInput
           name="username"
