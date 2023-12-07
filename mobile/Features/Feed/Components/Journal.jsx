36f685027b573d442d2eb13a6bcdc99c03a84ee0
Angela Tan <angelatan@Angelas-Air.lan>
2023-12-06 21:48:02 -0800

    pushing journal data to database

--- mobile/Features/Feed/Components/Journal.jsx
+++ mobile/Features/Feed/Components/Journal.jsx
@@ -1,58 +1,66 @@
 import {
-   ScrollView, Text, View, Button, TextInput, Keyboard, TouchableWithoutFeedback, Modal, TouchableOpacity, KeyboardAvoidingView
-  } from 'react-native';
-  import PropTypes from 'prop-types';
-import styles from "./JournalStyle"
+  ScrollView, Text, View, Button, TextInput, Keyboard, TouchableWithoutFeedback, Modal, TouchableOpacity, KeyboardAvoidingView,
+} from 'react-native';
+// import PropTypes from 'prop-types';
 import React, { useState } from 'react';
+import axios from 'axios';
+import styles from './JournalStyle';
 
-  
 export default function Journal() {
-const [text, setText] = useState('')
-const [confirmPopUp, setConfirmPopUp] = useState(false)
-const handlePopUp = () => {
-    setConfirmPopUp(!confirmPopUp)
-}
+  const [text, setText] = useState('');
+  const [confirmPopUp, setConfirmPopUp] = useState(false);
 
-const handleSubmit = () => {
-    console.log(text)
-}
+  const handlePopUp = () => {
+    setConfirmPopUp(!confirmPopUp);
+  };
 
-return (
+  const prompt = 'Create a journal post!';
+  const username = 'Nicole';
+
+  const addNewJournal = async (newUsername, newPrompt, newText) => {
+    const currentdate = new Date();
+    const timestamp = currentdate;
+    const res = await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/journals/createJournal`, { 'username': newUsername, 'prompt': newPrompt, 'text': newText, 'timestamp': timestamp });
+    handlePopUp();
+    console.log(res);
+  };
+
+  return (
     <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <View style={styles.container}>
-                <Text style={styles.prompt}>Create a journal post!</Text>
-                <View style={styles.textBox}>
-                    <ScrollView automaticallyAdjustKeyboardInsets={true}>
-                        <TextInput
-                            multiline={true}
-                            placeholder="Type your response"
-                            onChangeText={setText}
-                            value={text}
-                        />
-                        <View style={{ height: 40 }} />
-                    </ScrollView>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <View style={styles.container}>
+          <Text style={styles.prompt}>{prompt}</Text>
+          <View style={styles.textBox}>
+            <ScrollView automaticallyAdjustKeyboardInsets>
+              <TextInput
+                multiline
+                placeholder="Type your response"
+                onChangeText={setText}
+                value={text}
+              />
+              <View style={{ height: 40 }} />
+            </ScrollView>
+          </View>
+          <Button title="Submit" onPress={handlePopUp} />
+          <Modal visible={confirmPopUp} transparent>
+            <TouchableOpacity onPressOut={handlePopUp} style={styles.modalView}>
+              <View style={styles.modal}>
+                <Text>Confirm Submission?</Text>
+                <View style={{ flexDirection: 'row' }}>
+                  <Button title="Yes" onPress={() => addNewJournal(username, prompt, text)} />
+                  <Button title="No" onPress={handlePopUp} />
                 </View>
-                <Button title="Submit" onPress = {handlePopUp}></Button>
-                <Modal visible={confirmPopUp} transparent={true}>
-                    <TouchableOpacity onPressOut ={handlePopUp} style={styles.modalView}>
-                        <View style={styles.modal}>
-                            <Text>Confirm Submission?</Text>
-                            <View style={{flexDirection: 'row'}}>
-                                <Button title='Yes' onPress={handleSubmit}></Button>
-                                <Button title='No' onPress={handlePopUp}></Button>
-                            </View>
-                        </View>
-                    </TouchableOpacity>
-                </Modal>
-            
-            </View>
-            
+              </View>
+            </TouchableOpacity>
+          </Modal>
+
         </View>
+
+      </View>
     </TouchableWithoutFeedback>
-);
+  );
 }
-  
+
 //   Post.propTypes = {
 //     username: PropTypes.string.isRequired,
 //     body: PropTypes.string.isRequired,
@@ -61,4 +69,3 @@ return (
 //       navigate: PropTypes.func,
 //     }).isRequired,
 //   };
-  
--- mobile/Features/Feed/Screens/JournalPage.jsx
+++ mobile/Features/Feed/Screens/JournalPage.jsx
@@ -1,14 +1,7 @@
-import { useState, useEffect } from 'react';
-import {
-  Button, Text, View, TextInput,
-} from 'react-native';
-import PropTypes from 'prop-types';
 import Journal from '../Components/Journal';
 
-export default function JournalPage({ navigation }) {
-    return(
-          <Journal></Journal>
-
-    )
-
-};
+export default function JournalPage() {
+  return (
+    <Journal />
+  );
+}
