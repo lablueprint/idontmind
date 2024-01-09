f8ceeab5445d492d1244d16222c7e0b40d0739fb
Angela Tan <angelatan@wifi-131-179-0-239.host.ucla.edu>
2024-01-08 22:48:55 -0800

    dropdown

--- mobile/Features/Feed/Screens/Trends.jsx
+++ mobile/Features/Feed/Screens/Trends.jsx
@@ -1,9 +1,11 @@
-import { Button, Text, View } from 'react-native';
+import useState from 'react';
+import { View } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Trends() {
-  const [isWeek, setIsWeek] = useState(True);
-
+  // dropdown states
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
   const [items, setItems] = useState([
     { label: 'week', value: 'week' },
     { label: 'month', value: 'month' },
@@ -12,16 +14,16 @@ export default function Trends() {
   return (
     <View>
       <DropDownPicker
-      open={open}
-      value={value}
-      items={items}
-      setOpen={setOpen}
-      setValue={setValue}
-      setItems={setItems}
-      style={{ width: '90%', alignSelf: 'center' }}
-      drop
-
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+        style={{ width: '90%', alignSelf: 'center' }}
+        dropDownContainerStyle={{ width: '90%', alignSelf: 'center' }}
       />
+
     </View>
   );
 }
