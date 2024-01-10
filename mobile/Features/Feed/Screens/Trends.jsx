48365bc47490b8ce696b35beb8b96e582054da65
Joyce Chen <joyce.chen721@gmail.com>
2024-01-09 17:36:46 -0800

    get change weeks logic to work

--- mobile/Features/Feed/Screens/Trends.jsx
+++ mobile/Features/Feed/Screens/Trends.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text } from 'react-native';
+import { Button, View, Text } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function Trends() {
@@ -10,6 +10,24 @@ export default function Trends() {
     { label: 'week', value: 'week' },
     { label: 'month', value: 'month' },
   ]);
+  // -1 = last week, 0 = this week, 1 = next week, etc.
+  const [weekDisplay, setWeekDisplay] = useState(0);
+
+  // changes the week when navigating the < > buttons
+  const changeWeek = (val) => {
+    if (weekDisplay + val > 0) return;
+    setWeekDisplay(weekDisplay + val);
+  };
+
+  // map week number to displayed text
+  const getWeekText = () => {
+    if (weekDisplay === 0) {
+      return 'this week.';
+    } if (weekDisplay === -1) {
+      return 'last week';
+    }
+    return `${Math.abs(weekDisplay)} weeks ago`;
+  };
 
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
@@ -25,6 +43,16 @@ export default function Trends() {
       />
       <Text>trends</Text>
       <Text>your tendencies in this past week</Text>
+      <View style={{
+        flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center',
+      }}
+      >
+        <Button title="<" color="black" onPress={() => changeWeek(-1)} />
+        <Text>
+          {getWeekText()}
+        </Text>
+        <Button title=">" color="black" onPress={() => changeWeek(1)} />
+      </View>
     </View>
   );
 }
