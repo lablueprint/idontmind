e619ff11fbee0ce4237fc8e518c0b5e79209ccd8
ChancellorO <chancellororichey@gmail.com>
2024-01-10 20:22:17 -0800

    Added style for grid

--- mobile/Features/Feed/Screens/ContentLibrary.jsx
+++ mobile/Features/Feed/Screens/ContentLibrary.jsx
@@ -5,14 +5,43 @@ import {
 import PropTypes from 'prop-types';
 import axios from 'axios';
 
+const styles = {
+  app: {
+    flex: 4, // the number of columns you want to devide the screen into
+    marginTop: 30,
+  },
+  row: {
+    flexDirection: 'row',
+  },
+  '1col': {
+    backgroundColor: 'lightblue',
+    borderColor: '#fff',
+    borderWidth: 1,
+    flex: 1,
+  },
+  '2col': {
+    backgroundColor: 'purple',
+    borderColor: '#fff',
+    borderWidth: 1,
+    flex: 1,
+  },
+};
+
 export default function ContentLibrary({ navigation }) {
   const navigateToLanding = () => {
     navigation.navigate('Content Library');
   };
 
   return (
-    <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
-      <Text>Content</Text>
+    <View style={styles.app}>
+      <View style={styles.row}>
+        <View style={styles['1col']}>
+          <Text>Content</Text>
+        </View>
+        <View style={styles['2col']}>
+          <Text>Content</Text>
+        </View>
+      </View>
       <Button
         title="To Landing"
         onPress={navigateToLanding}
