f7938e145d37bbed02285958dcd7abfd60436468
ChancellorO <chancellororichey@gmail.com>
2024-01-10 22:53:51 -0800

    added carousel implementation

--- mobile/Features/Feed/Screens/ContentLibrary.jsx
+++ mobile/Features/Feed/Screens/ContentLibrary.jsx
@@ -1,51 +1,116 @@
 import { useState, useEffect } from 'react';
 import {
-  Button, Text, View, TextInput,
+  Button, Text, View, TextInput, StyleSheet, TouchableOpacity, FlatList,
 } from 'react-native';
 import PropTypes from 'prop-types';
-import axios from 'axios';
+// import axios from 'axios';
 
-const styles = {
-  app: {
-    flex: 4, // the number of columns you want to devide the screen into
-    marginTop: 30,
+/* Style Sheet */
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'yellow',
   },
   row: {
     flexDirection: 'row',
   },
-  '1col': {
-    backgroundColor: 'lightblue',
-    borderColor: '#fff',
-    borderWidth: 1,
-    flex: 1,
+  button: {
+    backgroundColor: 'lightgreen',
+    borderRadius: 20,
   },
-  '2col': {
-    backgroundColor: 'purple',
-    borderColor: '#fff',
-    borderWidth: 1,
-    flex: 1,
+});
+
+/* Fake Data */
+const DATA = [
+  {
+    id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+    title: 'First Item',
   },
-};
+  {
+    id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
+    title: 'Second Item',
+  },
+  {
+    id: '58694a0f-3da1-471f-bd96-145571e29d72',
+    title: 'Third Item',
+  },
+  {
+    id: 'bd7acbea-c1b1-42c2-aed5-3ad53abb28ba',
+    title: 'Forth Item',
+  },
+  {
+    id: '3ac68afc-c605-46d3-a4f8-fbd91aa97f63',
+    title: 'Fifth Item',
+  },
+  {
+    id: '58694a0f-3da2-471f-bd96-145571e29d72',
+    title: 'Sixth Item',
+  },
+];
 
 export default function ContentLibrary({ navigation }) {
+  const [selected, setSelected] = useState('false');
   const navigateToLanding = () => {
     navigation.navigate('Content Library');
   };
 
+  const renderItem = (item) => {
+    const backgroundColor = 'lightgreen';
+    const color = 'black';
+    return (
+      <TouchableOpacity
+        style={[style.button, {
+          padding: 20, marginVertical: 8, marginHorizontal: 16,
+        }]}
+      >
+        <Text
+          style={{ backgroundColor, color, height: 100 }}
+        >
+          Resource Name
+        </Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
-    <View style={styles.app}>
-      <View style={styles.row}>
-        <View style={styles['1col']}>
-          <Text>Content</Text>
+    <View
+      style={[style.container, { paddingLeft: 20 }]}
+    >
+      <View style={[style.row, { paddingTop: 100, flexBasis: 150, backgroundColor: 'lightgreen' }]}>
+        <Text style={{ fontSize: 28, flex: 2 }}>Content</Text>
+        <View
+          style={[style.container, { flex: 2, alignItems: 'center' }]}
+        >
+          <TouchableOpacity
+            onPress={() => setSelected('True')}
+            style={[style.button, { flexBasis: 30, justifyContent: 'center' }]}
+          >
+            <Text style={{ textAlign: 'center' }}>favorites</Text>
+
+          </TouchableOpacity>
         </View>
-        <View style={styles['2col']}>
-          <Text>Content</Text>
-        </View>
+        <Text style={{ flex: 1 }}>stuff</Text>
       </View>
-      <Button
-        title="To Landing"
-        onPress={navigateToLanding}
+      <View style={[style.row, { flexBasis: 35, backgroundColor: 'lightblue' }]}>
+        <Text style={{ fontSize: 15, flex: 1 }}>recommended for you</Text>
+      </View>
+      <View style={[style.row, { flex: 1, backgroundColor: 'lightgrey' }]}>
+        <FlatList
+          horizontal
+          data={DATA}
+          renderItem={renderItem}
+        />
+      </View>
+      <View
+        style={{ borderBottomColor: 'grey', borderBottomWidth: 1 }}
       />
+      <View style={[style.row, { flex: 2, backgroundColor: 'lightgrey', paddingTop: 25 }]}>
+        <FlatList
+          data={DATA}
+          renderItem={renderItem}
+        />
+      </View>
     </View>
   );
 }
