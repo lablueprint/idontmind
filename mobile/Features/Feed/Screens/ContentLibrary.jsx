6aeea023fb827100007c69d178d727f7c7b58c20
Chance Richey <crichey57@g.ucla.edu>
2024-01-16 16:49:08 -0800

    Created ContentLibrary Screen

--- mobile/Features/Feed/Screens/ContentLibrary.jsx
+++ mobile/Features/Feed/Screens/ContentLibrary.jsx
@@ -1,23 +1,48 @@
-import { useState, useEffect } from 'react';
 import {
-  Button, Text, View, TextInput, StyleSheet, TouchableOpacity, FlatList,
+  Text, View, StyleSheet, TouchableOpacity, FlatList, Image,
 } from 'react-native';
 import PropTypes from 'prop-types';
-// import axios from 'axios';
 
 /* Style Sheet */
 const style = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'yellow',
+    backgroundColor: 'white',
   },
   row: {
     flexDirection: 'row',
   },
   button: {
-    backgroundColor: 'lightgreen',
+    backgroundColor: 'white',
     borderRadius: 20,
+  },
+  horizontalCard: {
+    padding: 10, marginVertical: 8, marginRight: 16, backgroundColor: 'lightgrey', borderRadius: 10, width: 122, flex: 1,
+  },
+  star: {
+    width: 20, height: 20, opacity: 0.4,
+  },
+  horizontalCardInfo: {
+    flexDirection: 'row', flex: 1, width: 70,
+  },
+  horizontalText: {
+    color: 'black', alignSelf: 'flex-end', marginBottom: 20, flexWrap: 'wrap', flex: 1, fontSize: 16, marginLeft: 5,
+  },
+  verticalCard: {
+    padding: 10, marginVertical: 8, marginRight: 16, backgroundColor: 'lightgrey', borderRadius: 10, flex: 1, flexDirection: 'column',
+  },
+  shape: {
+    width: 95, height: 85, marginRight: 15,
+  },
+  verticalCardInfo: {
+    flex: 1, flexDirection: 'row-reverse',
+  },
+  verticalText: {
+    fontSize: 25,
+  },
+  whiteBox: {
+    backgroundColor: 'white', width: 100, height: 25, borderRadius: 8, marginBottom: 5, marginLeft: 5,
   },
 });
 
@@ -25,90 +50,138 @@ const style = StyleSheet.create({
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    title: 'First Item',
+    title: 'resource name',
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    title: 'Second Item',
+    title: 'resource name',
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    title: 'Third Item',
+    title: 'resource name',
   },
   {
     id: 'bd7acbea-c1b1-42c2-aed5-3ad53abb28ba',
-    title: 'Forth Item',
+    title: 'resource name',
   },
   {
     id: '3ac68afc-c605-46d3-a4f8-fbd91aa97f63',
-    title: 'Fifth Item',
+    title: 'resource name',
   },
   {
     id: '58694a0f-3da2-471f-bd96-145571e29d72',
-    title: 'Sixth Item',
+    title: 'resource name',
   },
 ];
 
 export default function ContentLibrary({ navigation }) {
-  const [selected, setSelected] = useState('false');
   const navigateToLanding = () => {
-    navigation.navigate('Content Library');
+    navigation.navigate('Landing');
   };
 
-  const renderItem = (item) => {
-    const backgroundColor = 'lightgreen';
-    const color = 'black';
-    return (
-      <TouchableOpacity
-        style={[style.button, {
-          padding: 20, marginVertical: 8, marginHorizontal: 16,
-        }]}
+  const horizontalRenderItem = ({ item }) => (
+    <TouchableOpacity
+      style={[style.horizontalCard]}
+    >
+      <Image
+        style={[style.star,
+          { alignSelf: 'flex-end' },
+        ]}
+        source={require('../../../assets/star.png')}
+      />
+      <View
+        style={[style.horizontalCardInfo]}
       >
         <Text
-          style={{ backgroundColor, color, height: 100 }}
+          style={[style.horizontalText]}
         >
-          Resource Name
+          {item.title}
         </Text>
-      </TouchableOpacity>
-    );
-  };
+      </View>
+    </TouchableOpacity>
+  );
 
-  return (
-    <View
-      style={[style.container, { paddingLeft: 20 }]}
+  const verticalRenderItem = ({ item }) => (
+    <TouchableOpacity
+      style={[style.verticalCard]}
     >
-      <View style={[style.row, { paddingTop: 100, flexBasis: 150, backgroundColor: 'lightgreen' }]}>
-        <Text style={{ fontSize: 28, flex: 2 }}>Content</Text>
-        <View
-          style={[style.container, { flex: 2, alignItems: 'center' }]}
-        >
-          <TouchableOpacity
-            onPress={() => setSelected('True')}
-            style={[style.button, { flexBasis: 30, justifyContent: 'center' }]}
-          >
-            <Text style={{ textAlign: 'center' }}>favorites</Text>
-
-          </TouchableOpacity>
-        </View>
-        <Text style={{ flex: 1 }}>stuff</Text>
-      </View>
-      <View style={[style.row, { flexBasis: 35, backgroundColor: 'lightblue' }]}>
-        <Text style={{ fontSize: 15, flex: 1 }}>recommended for you</Text>
-      </View>
-      <View style={[style.row, { flex: 1, backgroundColor: 'lightgrey' }]}>
-        <FlatList
-          horizontal
-          data={DATA}
-          renderItem={renderItem}
+      <Text
+        style={[style.verticalText]}
+      >
+        {item.title}
+      </Text>
+      <View style={[style.verticalCardInfo]}>
+        <Image
+          style={[style.shape]}
+          source={require('../../../assets/shape.png')}
         />
       </View>
       <View
-        style={{ borderBottomColor: 'grey', borderBottomWidth: 1 }}
+        style={[style.whiteBox]}
       />
-      <View style={[style.row, { flex: 2, backgroundColor: 'lightgrey', paddingTop: 25 }]}>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View
+      style={[style.container, { paddingLeft: 25 }]}
+    >
+      <View style={[style.row, { paddingTop: 75, flexBasis: 125, backgroundColor: 'white' }]}>
+        <Text style={{ fontSize: 30, flex: 3, paddingLeft: 5, }}>content</Text>
+        <View
+          style={[style.container, { flex: 3 }]}
+        >
+          <TouchableOpacity
+            onPress={() => navigateToLanding}
+            style={[style.button, {
+              flexBasis: 37, justifyContent: 'center', backgroundColor: 'lightgray', width: 110, flexDirection: 'row',
+            }]}
+          >
+            <Image
+              style={{
+                width: 20, height: 20, marginTop: 7, marginRight: 3, opacity: 0.4,
+              }}
+              source={require('../../../assets/star.png')}
+            />
+            <Text style={{
+              textAlign: 'center', fontSize: 16, marginTop: 9, marginRight: 2,
+            }}
+            >
+              favorites
+            </Text>
+
+          </TouchableOpacity>
+        </View>
+        <View style={{ flex: 2, flexDirection: 'row', marginTop: 5 }}>
+          <Image
+            style={{ width: 20, height: 31, marginRight: 15 }}
+            source={require('../../../assets/search.png')}
+          />
+          <Image
+            style={{ width: 20, height: 31 }}
+            source={require('../../../assets/filter.png')}
+          />
+        </View>
+      </View>
+      <View style={[style.row, { flexBasis: 35 }]}>
+        <Text style={{ fontSize: 16, flex: 1, color: 'gray' }}>recommended for you</Text>
+      </View>
+      <View style={[style.row, { flex: 1 }]}>
+        <FlatList
+          horizontal
+          data={DATA}
+          renderItem={horizontalRenderItem}
+          showsHorizontalScrollIndicator={false}
+        />
+      </View>
+      <View
+        style={{ borderBottomColor: 'grey', borderBottomWidth: 0.75, opacity: 0.25 }}
+      />
+      <View style={[style.row, { flex: 2, paddingTop: 25 }]}>
         <FlatList
           data={DATA}
-          renderItem={renderItem}
+          renderItem={verticalRenderItem}
+          showsVerticalScrollIndicator={false}
         />
       </View>
     </View>
