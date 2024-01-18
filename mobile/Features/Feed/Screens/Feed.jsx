41533641f526dd4d662a8d6ff42f13e90d09bdff
Aaron Shi <ashi53@g.ucla.edu>
2024-01-17 18:27:10 -0800

    post a little less messy

--- mobile/Features/Feed/Screens/Feed.jsx
+++ mobile/Features/Feed/Screens/Feed.jsx
@@ -10,48 +10,16 @@ export default function Feed({ navigation }) {
   const [postDraftBody, setPostDraftBody] = useState('');
   const [postDraftUser, setPostDraftUser] = useState('');
 
-  // const GIVEN_POSTS = [
-  //   {
-  //     _id: 1,
-  //     username: 'James',
-  //     body: 'Mobile development is fun!',
-  //   },
-  //   {
-  //     _id: 2,
-  //     username: 'Sidd',
-  //     body: 'I just finished watching another movie. It was interesting, but kind of boring :(',
-  //   },
-  //   {
-  //     _id: 3,
-  //     username: 'Jerry',
-  //     body: 'I am excited to see everyone become friends!',
-  //   },
-  // ];
-
   const [postList, setPostList] = useState([]);
   const navigateToLanding = () => {
     navigation.navigate('Landing');
-  };
-
-  const navigateToCheckIn = () => {
-    navigation.navigate('CheckIn');
   };
 
   useEffect(() => {
     const foo = async () => {
       try {
         const res = await axios.get(`${process.env.EXPO_PUBLIC_SERVER_URL}/posts/fetchPosts`);
-        // const res2 = await axios.get('http://localhost:4000/posts/fetchPosts');
-        // // const squirreltime = new Date(res.data[5].timestamp);
-
-        // const testdate = `Last Sync: ${squirreltime.getDate()}/${
-        //   squirreltime.getMonth() + 1}/${
-        //   squirreltime.getFullYear()} @ ${
-        //   squirreltime.getHours()}:${
-        //   squirreltime.getMinutes()}:${
-        //   squirreltime.getSeconds()}`;
         setPostList(res.data);
-        // console.log(testdate);
       } catch (err) {
         console.log(err);
       }
@@ -63,21 +31,10 @@ export default function Feed({ navigation }) {
     const foo = async () => {
       try {
         await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/posts/createPost`, data);
-        // console.log(res.data);
       } catch (err) {
         console.log(err);
       }
     };
-
-    // const currentdate = new Date();
-    // const datetime = `Last Sync: ${currentdate.getDate()}/${
-    //   currentdate.getMonth() + 1}/${
-    //   currentdate.getFullYear()} @ ${
-    //   currentdate.getHours()}:${
-    //   currentdate.getMinutes()}:${
-    //   currentdate.getSeconds()}`;
-    // console.log(currentdate);
-    // console.log(datetime);
     foo();
   };
 
@@ -92,19 +49,20 @@ export default function Feed({ navigation }) {
     setPostList(tempPostList);
     setPostDraftBody('');
     setPostDraftUser('');
+    const currentDate = new Date();
+    const timestamp = currentDate.getTime();
     pushPosts({
       username: postDraftUser,
       body: postDraftBody,
+      timestamp,
     });
   };
 
   const onChangeDraftBody = (val) => {
-    // console.log(val);
     setPostDraftBody(val);
   };
 
   const onChangeDraftUser = (val) => {
-    // console.log(val);
     setPostDraftUser(val);
   };
 
@@ -144,11 +102,6 @@ export default function Feed({ navigation }) {
       <Button
         title="To Landing"
         onPress={navigateToLanding}
-      />
-       {/* added for testing */}
-      <Button
-        title="To CheckIn"
-        onPress={navigateToCheckIn}
       />
     </View>
   );
