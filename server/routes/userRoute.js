98dfa11836730bcfa69cddb8060999e45f8f2e18
Antonio Cosio <coshio@g.ucla.edu>
2024-01-29 21:51:22 -0800

    Moved authentication to the top of user routes

--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,10 +3,10 @@ const express = require('express');
 const userRouter = express.Router();
 const userController = require('../controllers/userController');
 
+userRouter.use(userController.authenticatePassport)
 userRouter.post('/signin', userController.signInUser);
 userRouter.post('/signup', userController.signUpUser);
 userRouter.post('/getData', userController.getUserData);
-userRouter.use(userController.authenticatePassport)
 
 userRouter.get('/get', (req, res) => {
   res.send('Get API');
