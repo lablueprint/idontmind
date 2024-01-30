327ba3b29c7e1791066ab9f1442645f2b3a586be
Joyce Chen <joyce.chen721@gmail.com>
2024-01-29 21:19:42 -0800

    stack navigator for favorites screen (bottom tabs not showing tho)

--- mobile/Navigation/AppNavigation.jsx
+++ mobile/Navigation/AppNavigation.jsx
@@ -1,16 +1,16 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigationBar from './NavigationBar';
 import Favorites from '../Features/Other/Screens/Favorites';
 
-const Stack = createStackNavigator();
-
 export default function AppNavigation() {
+  const Stack = createNativeStackNavigator();
+
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen
-          name="MainTabs"
+          name="NavigationBar"
           component={NavigationBar}
           options={{ headerShown: false }}
         />
