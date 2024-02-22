6dfac4a5433b48fb37fe5799e2733914e574c37f
Antonio Cosio <coshio@g.ucla.edu>
2024-02-21 23:04:17 -0800

    Hardcoded sign in for landing page

--- mobile/Features/Landing/Screens/Landing.jsx
+++ mobile/Features/Landing/Screens/Landing.jsx
@@ -2,14 +2,39 @@ import {
   Button, Text, View,
 } from 'react-native';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+import { useDispatch } from 'react-redux';
+import { login } from '../../../redux/authSlice';
 
 export default function Landing({ navigation }) {
   const navigateToLogin = () => {
     navigation.navigate('Login');
   };
-
   const navigateToSignUp = () => {
     navigation.navigate('SignUp');
+  };
+  const navigateToFeed = () => {
+    navigation.navigate('Feed');
+  }
+  
+  const dispatch = useDispatch();
+
+  const handleHardcodedLogin = async () => {
+    try {
+      const userData = {
+        email: 'poop@gmail.com',
+        password: 'poop',
+      };
+      const res = await axios.post(`${process.env.EXPO_PUBLIC_SERVER_URL}/users/signin`, userData);
+      if (res.data.error) {
+        console.error(res.data.error);
+      } else {
+        dispatch(login(res.data));
+        navigateToFeed();
+      }
+    } catch (err) {
+      console.error(err.message);
+    }
   };
 
   return (
@@ -22,6 +47,10 @@ export default function Landing({ navigation }) {
       <Button
         title="Already have an account?"
         onPress={navigateToLogin}
+      />
+      <Button
+        title="Hardcoded Sign In"
+        onPress={handleHardcodedLogin}
       />
     </View>
   );
