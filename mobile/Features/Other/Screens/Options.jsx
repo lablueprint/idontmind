82d6a35c9071c0d2ada060224c94dba5c9aaee12
Angela Tan <angelatan@Angelas-MacBook-Air.local>
2024-02-21 22:01:06 -0800

    added image upload

--- mobile/Features/Other/Screens/Options.jsx
+++ mobile/Features/Other/Screens/Options.jsx
@@ -1,4 +1,8 @@
-import { View, Text, TouchableOpacity } from 'react-native';
+import {
+  ScrollView, View, Text, TouchableOpacity, Image, Button, StyleSheet,
+} from 'react-native';
+import { useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 import PropTypes from 'prop-types';
 import OptionStyle from './OptionStyle';
 
@@ -13,9 +17,43 @@ export default function Options({ navigation }) {
       navigation.navigate('BannedTags');
     }
   };
+  const [image, setImage] = useState(null);
+
+  const uploadPhotoStyles = StyleSheet.create({
+    container: {
+      height: '10%',
+      width: '90%',
+      borderRadius: 10,
+      backgroundColor: 'lightgrey',
+      alignItems: 'center',
+      justifyContent: 'center',
+      marginBottom: 16,
+    },
+    image: {
+      width: 100,
+      height: 100,
+      marginBottom: 16,
+      borderRadius: 50,
+    },
+  });
+
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setSelectedImage(result.assets[0].uri);
+    }
+  };
 
   return (
-    <View style={OptionStyle.container}>
+    <ScrollView contentContainerStyle={OptionStyle.container}>
       <Text style={OptionStyle.title}> options </Text>
       <Text style={OptionStyle.personalData}> personal data </Text>
       {options.map((option, index) => (
@@ -32,7 +70,20 @@ export default function Options({ navigation }) {
           </View>
         </View>
       ))}
-    </View>
+      {/* <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button title="Pick an image from camera roll" onPress={pickImage} />
+        {image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
+      </View> */}
+      {selectedImage !== '' ? (
+        <Image
+          source={{ uri: selectedImage }}
+          style={{ width: 200, height: 200 }}
+        />
+      ) : ''}
+      <TouchableOpacity style={uploadPhotoStyles.container} onPress={pickImage}>
+        <Text>+</Text>
+      </TouchableOpacity>
+    </ScrollView>
 
   );
 }
