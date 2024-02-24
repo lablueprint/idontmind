467ca85d70789490d15631c3d7b811ca6d46ca0a
Joyce Chen <joyce.chen721@gmail.com>
2024-02-23 21:13:22 -0800

    retrieve image in frontend & upload image controller

--- server/controllers/awsController.js
+++ server/controllers/awsController.js
@@ -23,6 +23,24 @@ const retrieveImage = async (req, res) => {
   }
 };
 
+const uploadImage = async (req, res) => {
+  try {
+    const params = {
+      Bucket: process.env.S3_BUCKET,
+      Key: 'uploaded_image.jpeg', // Key is the name of the file in the bucket
+      Body: req.body.imageData, // Assuming imageData contains the image data
+      ContentType: 'image/jpeg', // Set the content type according to your image type
+    };
+
+    await s3.putObject(params).promise(); // Use promise() to make the call asynchronous
+
+    res.send('Image uploaded successfully');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error uploading image');
+  }
+};
+
 module.exports = {
-  retrieveImage,
+  retrieveImage, uploadImage,
 };
