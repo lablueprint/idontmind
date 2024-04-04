acc235eec10eb4139bc2ccae85176e9084884706
Nicole Ju <nicoleju@g.ucla.edu>
2024-04-03 22:25:32 -0700

    moved journal history, brought back contentstyle because i deleted the wrong one

--- mobile/Features/Journal/Components/JournalHistoryPage.jsx
+++ mobile/Features/Journal/Components/JournalHistoryPage.jsx
@@ -4,7 +4,7 @@ import {
 import PropTypes from 'prop-types';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import JournalCard from '../../Journal/Components/JournalCard';
+import JournalCard from './JournalCard';
 
 export default function JournalHistoryPage({ navigation }) {
   const [journals, setJournals] = useState([]); // array of journal entries
--- mobile/Navigation/NavigationBar.jsx
+++ mobile/Navigation/NavigationBar.jsx
@@ -8,7 +8,7 @@ import ContentLibrary from '../Features/ContentLibrary/Screens/ContentLibrary';
 import PushNotifications from '../Features/Settings/Screens/PushNotifications';
 import FindHelp from '../Features/Other/Screens/FindHelp';
 import Options from '../Features/Other/Screens/Options';
-import JournalHistoryPage from '../Features/Other/Screens/JournalHistoryPage';
+import JournalHistoryPage from '../Features/Journal/Components/JournalHistoryPage';
 import WOYM from '../Features/Register/WOYM';
 import DayChallenge from '../Features/Other/Screens/DayChallenge';
 import ContentDashboard from '../Features/Other/Screens/ContentDashboard';
