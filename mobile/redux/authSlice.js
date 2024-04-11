040b318b9b3ee9327f9e16057cdb0433d187d06a
Antonio Cosio <coshio@g.ucla.edu>
2024-04-11 09:50:44 +0900

    skeleten for secure store implementation

--- mobile/redux/authSlice.js
+++ mobile/redux/authSlice.js
@@ -1,5 +1,13 @@
 /* eslint-disable no-param-reassign */
+import * as SecureStore from 'expo-secure-store';
+import jwt_decode from 'jwt-decode';
+
 const { createSlice } = require('@reduxjs/toolkit');
+
+const user = SecureStore.getItemAsync('user');
+const token = SecureStore.getItemAsync('token');
+console.log(user);
+console.log(token);
 
 const initialState = {
   email: null,
@@ -17,6 +25,8 @@ const authSlice = createSlice({
       state.authHeader = {
         Authorization: `Bearer ${action.payload.token}`,
       };
+      SecureStore.setItemAsync('token', JSON.stringify(action.payload.token));
+      SecureStore.setItemAsync('user', JSON.stringify(action.payload.user));
     },
     logout: (state) => {
       state.id = null;
@@ -24,6 +34,8 @@ const authSlice = createSlice({
       state.firstName = null;
       state.token = null;
       state.authHeader = null;
+      SecureStore.deleteItemAsync('token');
+      SecureStore.deleteItemAsync('user');
     },
   },
 });
@@ -36,3 +48,9 @@ export const {
 const { reducer } = authSlice;
 
 export default reducer;
+
+export const isTokenExpired = (token) => {
+  const decodedToken = jwt_decode(token);
+  const currentTime = Date.now() / 1000;
+  return decodedToken.exp < currentTime;
+};
