12c18783f422de42d546ce3a036b0c27c1916433
Alan Nguyen <alannguyen@Alans-MacBook-Pro.local>
2024-05-20 14:58:50 -0700

    Attempted background

--- mobile/Features/GettingStarted/Splash.jsx
+++ mobile/Features/GettingStarted/Splash.jsx
@@ -1,8 +1,9 @@
 import {
-  Text, View, Pressable, TouchableOpacity, Image,
+  Text, View, Pressable, TouchableOpacity, Image, ImageBackground,
 } from 'react-native';
 import PropTypes from 'prop-types';
 import IDMlogo from '../../assets/images/idontmindlogo.png';
+import bg from '../../assets/images/splash-bg.jpeg';
 import styles from './SplashStyle';
 
 function Splash({ navigation }) {
@@ -11,31 +12,35 @@ function Splash({ navigation }) {
   };
 
   return (
-    <View style={styles.container}>
-      <Image source={IDMlogo} style={styles.logo} />
-      <Text style={styles.underLogo}>
-        The short-term relationship that&apos;s good for your mental health.
-      </Text>
-      <View style={styles.termsPrivacy}>
-        <Text style={styles.termsText}>By registering or signing in, you are agreeing to our</Text>
-        <View style={styles.termsNext}>
-          <TouchableOpacity onPress={navigateToTerms}>
-            <Text style={styles.buttonWord}>Terms of Service </Text>
-          </TouchableOpacity>
-          <Text>and</Text>
-          <TouchableOpacity onPress={navigateToTerms}>
-            <Text style={styles.buttonWord}> Privacy Policy</Text>
-          </TouchableOpacity>
-          <Text>.</Text>
+    <ImageBackground source={bg}>
+      <View style={styles.container}>
+        <Image source={IDMlogo} style={styles.logo} />
+        <Text style={styles.underLogo}>
+          The short-term relationship that&apos;s good for your mental health.
+        </Text>
+        <View style={styles.termsPrivacy}>
+          <Text style={styles.termsText}>
+            By registering or signing in, you are agreeing to our
+          </Text>
+          <View style={styles.termsNext}>
+            <TouchableOpacity onPress={navigateToTerms}>
+              <Text style={styles.buttonWord}>Terms of Service </Text>
+            </TouchableOpacity>
+            <Text>and</Text>
+            <TouchableOpacity onPress={navigateToTerms}>
+              <Text style={styles.buttonWord}> Privacy Policy</Text>
+            </TouchableOpacity>
+            <Text>.</Text>
+          </View>
         </View>
+        <Pressable style={styles.getStarted}>
+          <Text>Get Started</Text>
+        </Pressable>
+        <Pressable style={styles.signIn}>
+          <Text>Sign In</Text>
+        </Pressable>
       </View>
-      <Pressable style={styles.getStarted}>
-        <Text>Get Started</Text>
-      </Pressable>
-      <Pressable style={styles.signIn}>
-        <Text>Sign In</Text>
-      </Pressable>
-    </View>
+    </ImageBackground>
   );
 }
 
